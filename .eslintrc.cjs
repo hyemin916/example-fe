19b92d2e41fc5ec8198ae223fa299584637eb277
박혜민/백엔드개발팀 <hyemin@ktown4u.com>
2024-01-03 22:15:18 +0900

    update prettier, eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,11 +8,15 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'simple-import-sort', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
+    ],
+    'prettier/prettier': [
+      'error',
+      {usePrettierrc: true},
     ],
   },
 }
